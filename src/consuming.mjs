e23ff33fb01c57a29c4c3968cededf7da65c4230
heathercreighton <heather.creighton@gmail.com>
2021-02-12 16:51:48 -0500

    Update consuming.mjs

--- src/consuming.mjs
+++ src/consuming.mjs
@@ -1,16 +1,81 @@
-import setText, {appendText, showWaiting, hideWaiting} from "./results.mjs";
+import setText, {appendText, showWaiting, hideWaiting} from './results.mjs';
 
+// handling a fullfilled promise
 export function get() {
+    
+    axios.get("http://localhost:3000/orders/1")
+        .then(({ data }) => {
+           return setText(JSON.stringify(data));
+        });
+
 }
 
-export function getCatch() {
+
+// handling a rejected promise
+export function getCatch(){
+  return axios.get("http://localhost:3000/orders/123")
+    .then(({ data }) => {
+        setText(JSON.stringify(data));
+    })
+        .catch(err => setText(err));
+
+
 }
 
-export function chain() {
+export function chain(){
+    return axios.get("http://localhost:3000/orders/1")
+    .then(({ data }) => {
+    return axios.get(`http://localhost:3000/addresses/${data.shippingAddress}`);
+    })
+        .then(({ data }) => { 
+            setText(`City: ${data.city}`);
+        });
+
+
 }
 
 export function chainCatch() {
-}
+    axios
+        .get("http://localhost:3000/orders/1")
+        .then(({ data }) => {
+            return axios.get(`http://localhost:3000/addresses/${data.shippingAddress}`
+            );
+        })
+            .then(({ data }) => { 
+                setText(`City: ${data.city}`);
+            })
+        
+            .catch(err => {
+                setText(err);
+               
+            })
+    }
+
+
+
 
 export function final() {
+    showWaiting();
+    axios
+        .get("http://localhost:3000/orders/1")
+        .then(({ data }) => {
+            console.log("Data", data)
+            return axios.get(
+                `http://localhost:3000/addresses/${data.shippingAddress}`
+            );
+        })
+        .then(({ data }) => {
+            setText(`City: ${data.city}`);
+        })
+        .catch(err =>
+            setText(err))
+        .finally(() => {
+            setTimeout(() => {
+                hideWaiting();
+            }, 1000
+            );
+            setTimeout(() => { appendText(" -- completely done!")},2000);
+        });   
+            
+
 }
